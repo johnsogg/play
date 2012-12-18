24b2b50107974d929810d94cad09f70410e670cc
Gabe Johnson <gabe.johnson@gmail.com>
2012-12-17 18:33:42 -0700

    Commented my short term plans for the python unit testing. Will have to come back to this after I take a swipe at C++ testing.

--- py/test_tree.py
+++ py/test_tree.py
@@ -3,18 +3,36 @@ import unittest
 from tree import Tree, Node
 
 class TestTree(unittest.TestCase):
+    """
+        When I pick this up again, this is what should be done:
+        
+        1. Make a special subclass of TestCase with the __init__ and __str__ 
+        functions as below. Call it RetroTestCase All my tests will subclass 
+        that one.
+        
+        2. Make an abstract class 'RetroSuite' that builds and runs the suite.
+        
+        3. Then, to make a concrete test set, subclass RetroSuite and supply
+        it with the RetroTestCase info (as done in the __main__ section below).
+    """
 
     def __init__(self, methodName, humanName, maxPts):
-        super(TestTree, self).__init__(methodName)
-        self.methodName = methodName
-        self.humanName = humanName
-        self.maxPts = maxPts
-        self.points = 0
+        super(TestTree, self).__init__(methodName) # call superconstructor
+        self.methodName = methodName # record method name, e.g. 'testInsert'
+        self.humanName = humanName # record user-legible name, e.g. "Insert Data"
+        self.maxPts = maxPts # maximum number of points for this test if passed
+        self.points = 0 # actual amount awarded (either 0 or maxPts)
 
     def setUp(self):
+        """
+            My RetroTestCase subclass will have to implement this.
+        """
         self.tree = Tree()
 
     def testInsert(self):
+        """
+            My RetroTestCase subclass will implement a bunch of testXYZ functions.
+        """
         self.assertEquals(self.tree.size(), 0)
         self.tree.insert(Node(7))
         self.assertEquals(self.tree.size(), 1)
@@ -22,13 +40,28 @@ class TestTree(unittest.TestCase):
         self.assertEquals(self.tree.size(), 12)
         self.assertEquals(self.tree.size(), 3)
 
+    def testRemove(self):
+        pass
+    
     def testNothing(self):
+        """
+            An existence function. RetroTestCase ought to have this one (don't
+            leave it to subclass) so students get a point for at least getting
+            their homework submitted.
+        """
         pass
 
     def __str__(self):
+        """
+            RetroTestCase includes this.
+        """
         return self.humanName
 
 class SpecialResult(unittest.TestResult):
+    """
+        RetroResult will basically be just like this, assuming 'test' is a
+        RetroTestCase.
+    """
     def addSuccess(self, test):
         print ("Test successful: " + str(test))
         test.points = test.maxPts
@@ -40,14 +73,22 @@ class SpecialResult(unittest.TestResult):
         print ("Test error: " + str(test) + ": " + str(err))
 
 if __name__ == '__main__':
-    suite = unittest.TestSuite()
-    tests = [
-        TestTree('testNothing', 'Existence', 1), 
-        TestTree('testInsert', 'Insert Data', 4),
+    """
+        This is the controller, RetroSuite. Parts are abstract (so they will be in
+        the RetroSuite class) and others are concrete (will be implemented in the
+        homework assignment's RetroSuite subclass implementation). They are marked
+        accordingly below.
+    """
+    suite = unittest.TestSuite() # abstract
+    tests = [ # concrete. This list is established in the subclass constructor.
+             TestTree('testNothing', 'Existence', 1),  # exclude this one, though
+             TestTree('testInsert', 'Insert Data', 4), # include this and friends
+             TestTree('testRemove', 'Remove Data', 4), 
         ]
+    # Everything below here is abstract ---------------------------------
     for test in tests:
         suite.addTest(test)
-
+    
     result = SpecialResult()
     suite.run(result)
     maxPoints = 0
